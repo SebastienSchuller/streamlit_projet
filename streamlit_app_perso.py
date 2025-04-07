fb6baa586234ee72282612cd8d1e8e0900b57995
SebastienSchuller <sebastien.schuller@orange.com>
2025-04-07 11:48:26 +0200

    App structure init

--- streamlit_app_perso.py
+++ streamlit_app_perso.py
@@ -1,3 +1,33 @@
 import streamlit as st
 
-st.write("Appli perso...")
+st.title("Analyse des commentaires clients")
+
+# Structure des pages
+st.sidebar.title("Sommaire")
+pages=["Présentation","Exploration", "Feature Engineering", "DataVisualisation", "Simulation"]
+page=st.sidebar.radio("Aller vers", pages)
+st.sidebar.markdown("---")
+st.sidebar.write("Sébastien S")
+
+if page=="Présentation":
+    st.write("Présentation du projet")
+
+elif page=="Exploration":
+    st.write("Exploration des données")
+
+elif page=="DataVisualisation":    
+    st.write ("Datavisualisation")
+
+elif page=="Simulation":
+    st.write('Saisssez un commentaire à analyser avec le modèle')
+
+    # zone de saisie du commentaire à tester
+    inputcommentaire=st.text_input("Commentaire à analyser:","Super produit !")
+
+    # bouton de validation
+    if st.button("Analyser"):
+        st.markdown("---")
+        st.write("Commentaire:")
+        st.write(inputcommentaire)
+        st.write("Longueur:")
+        st.write(len(inputcommentaire))
