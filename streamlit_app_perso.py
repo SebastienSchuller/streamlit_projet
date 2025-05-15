4527254be58aa189928c8d40fed4e7738723de75
SebastienSchuller <sebastien.schuller@orange.com>
2025-05-15 10:43:32 +0200

    llm_reponse

--- streamlit_app_perso.py
+++ streamlit_app_perso.py
@@ -330,7 +330,7 @@ elif page=="Simulation LLM":
 
             st.write("## Utilisation d'un modèle Mistral AI avec une structured output")
             st.write("Modèle: mistral-large-latest")
-            st.write("Température: "+temperature)
+            st.write("Température: "+ str(temperature))
 
             original_fields = Eval_commentaire.model_fields
             original_types = get_type_hints(Eval_commentaire)
@@ -350,7 +350,8 @@ elif page=="Simulation LLM":
             ### début partie LLM à vérifier
             from langchain_mistralai import ChatMistralAI
             from langchain_core.prompts import ChatPromptTemplate   
-
+            from langchain.prompts import FewShotPromptTemplate
+            from langchain.prompts.prompt import PromptTemplate
 
             # LLM with function call
             llm = ChatMistralAI(model="mistral-large-latest",api_key=mistral_api_key,temperature=temperature)
@@ -389,7 +390,91 @@ elif page=="Simulation LLM":
 
             #### début partie réponse au commentaire
             st.write("## Réponse au commentaire avec une approche few shot example:")
-    
+            auto_examples=[{'input': 'Nom client: nan Commentaire:Je ne recommande pas Showroomprivé en '
+                        'Belgique . Service client : 0 ! En fonction des marques , il '
+                        'arrive que les produits arrivent abîmés , que des erreurs sur le '
+                        'produit soient faites . Surtout consultez bien leurs conditions '
+                        'générales avant d ’ acheter , elles mettent à votre charge les '
+                        'frais de retour de votre colis , notamment si le produit ne vous '
+                        'plaît pas par exemple et aussi dans d ’ autres cas .',
+                'output': 'Bonjour , Je suis sincèrement désolé de lire votre ressenti quant '
+                            'aux anomalies relatives à vos commande . Je vous présente mes '
+                            'excuses au nom de shwroomprive.com.Je vous informe avoir pris en '
+                            'charge votre réclamation et vous confirme que vous serez '
+                            "recontactée afin de m'entretenir avec vous de vive voix.Ayoub"},
+                {'input': 'Nom client: Vinciane Denne Commentaire:Je commande des capsules de '
+                        'cafe Dolce Gusto au prix de 20,66€/ boite + fdp . Dans mon panier '
+                        "le total est juste . J'effectue le payement sans trop me poser de "
+                        "questions puis je vais directement voir ce que j'ai payé car "
+                        "quelque chose me semble louche . Je constate que j'ai payé "
+                        "23,69€/boite . Je fais directement un mail auquel j'ai eu réponse "
+                        'dans les 24h pour me dire que le prix est bien de 23,69 mais sur '
+                        'le site il est toujours affichés 20,66€ . Je ne trouve pas ca '
+                        "normal d'afficher un prix et de pas le respecter.Normalement prix "
+                        'affichés = prix a payé même si ils se sont trompé .',
+                'output': 'Bonjour , Je fais suite à votre message concernant les prix de '
+                            'vos capsules de café proposés sur notre site.Je vous informe '
+                            'avoir pris en charge votre réclamation et vous confirme que vous '
+                            'serez recontactée afin de faire suite à votre requête.Ayoub'},
+                {'input': "Nom client: nan Commentaire:Le vêtement m ' a énormément déçu . Le "
+                        "tissu ne correspond pas à ce que j'espérais .",
+                'output': "Bonjour , Je suis sincèrement navré d'apprendre que votre "
+                            'commande ne vous aient pas apportée entière satisfaction . Je '
+                            'vous présente mes excuses pour ce désagrément . Je vous informe '
+                            'avoir pris en charge votre réclamation et vous confirme que vous '
+                            "serez recontactée aujourd'hui afin de m'entretenir avec vous de "
+                            'vive voix sur ce sujet . Ayoub'},
+                {'input': 'Nom client: nan Commentaire:Vêtements de très mauvaises qualités '
+                        '... dommage',
+                'output': "Bonjour , Je suis sincèrement navré d'apprendre que vos vêtements "
+                            'ne vous aient pas apportée entière satisfaction . Je vous '
+                            'présente mes excuses pour ce désagrément . Je vous informe avoir '
+                            'pris en charge votre réclamation et vous confirme que vous serez '
+                            "recontactée aujourd'hui afin de m'entretenir avec vous de vive "
+                            'voix sur ce sujet . Ayoub'},
+                {'input': 'Nom client: Jacqueline L . Commentaire:Marchandise conforme à la '
+                        'commande livraison plus longue que prévu',
+                'output': 'Bonjour , Un grand merci pour votre avis ! Bonne journée de la '
+                            "part de toute l'équipe.Ayoub"}]
+            
+            # Création de la classe de sortie structurée
+            class Reponse_commentaire(BaseModel):
+                '''Réponse à un commentaire'''
+                reponse: str = Field(description="Réponse au commentaire")
+
+            # Prompt
+            system = """Tu es un professionnel du service client après-vente, qui analyse et répond à des commentaires laissés par des clients suite à une commande.\n
+                        Tu t'inspires pour les réponses des exemples fournis le plus possible. 
+                """
+
+            structured_llm_evaluateur_with_example = llm.with_structured_output(Reponse_commentaire)
+
+            examples=auto_examples
+
+            # Template pour afficher chaque exemple
+            example_template = """
+            Input: {input}
+            Output: {output}
+            """
+
+            prompt_template = FewShotPromptTemplate(
+                examples=examples,
+                example_prompt=PromptTemplate(input_variables=["input", "output"], template=example_template),
+                prefix=system,
+                suffix="Input: {input}\nOutput:",
+                input_variables=["input"]
+            )
+
+            retrieval_grader_with_example = prompt_template | structured_llm_evaluateur_with_example
+
+            def reponse_with_example(commentaire,prenom):
+                question="Répond à ce commentaire: "+ commentaire + ". Nom:" + str(prenom)
+                docs = retrieval_grader_with_example.invoke({"input": question})
+                return docs
+            
+            retour=reponse_with_example(inputcommentaire,"")
+            
+            st.write(retour)
 
 
 
